d198aa55ae2a02239cfd548a2fe9894bfbb24c04
Ivan Zorin <creator@localzet.com>
2024-10-17 18:31:44 +0300

    Update

--- config/route.php
+++ config/route.php
@@ -1,8 +1,12 @@
 <?php
 
-use Triangle\Router;
 use support\Request;
+use Triangle\Router;
+
+Router::disableDefaultRoute();
 
 Router::any('/hello/{name}', function (Request $request, string $name) {
     return response("Привет, $name!");
 });
+
+Router::fallback(fn() => view('index'));
